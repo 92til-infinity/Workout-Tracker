4af0eeb51256afa84f0fc65a5bf85ff587d05004
Scott G <glover.scott92@gmail.com>
2020-10-17 15:36:53 -0700

    rework and correct  routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,58 +1,69 @@
 const db = require("../models/Excercises");
-const mongojs = require("mongojs");
+const router = require("express").Router();
 
-module.exports = function (app) {
-    app.get("/api/workouts", function (req, res) {
-        db.find()
-            .then(data => {
-                res.json(data)
-            })
-            .catch(err => {
-                res.json(err)
-            })
+router.post("/api/workouts", (req, res) => {
+    db.create()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+
+// router.put("/api/workouts", (req, res) => {
+//     db.create({})
+//         .then(data => {
+//             console.log(data);
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
+
+router.get("/api/workouts/range", function (req, res) {
+    db.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+
+router.put("/api/workouts/range", function (req, res) {
+    db.create({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    const id = mongojs.ObjectId(req.params.id);
+    db.find({ _id: id })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    db.findByIdAndDelete(body.id)
+        .then(() =>
+            res.json(true);
+})
+    .catch(err => {
+        res.json(err);
     });
-
-
-    app.put("/api/workouts", (req, res) => {
-        db.create({})
-            .then(data => {
-                console.log(data);
-                res.json(data);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-    app.get("/api/workouts/range", function (req, res) {
-        db.find()
-            .then(data => {
-                res.json(data)
-            })
-            .catch(err => {
-                res.json(err)
-            })
-    });
-
-
-    app.put("/api/workouts/range", function (req, res) {
-        db.create({})
-            .then(data => {
-                res.json(data);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-    app.put("/api/workouts/:id", (req, res) => {
-        const id = mongojs.ObjectId(req.params.id);
-        db.find({ _id: id })
-            .then((data) => {
-                res.json(data);
-            })
-            .catch((err) => {
-                res.json(err);
-            });
-    });
+    
 }
+
+module.exports = router;
